88dfa48821950f952e3fc42b51f9b46ed9f4469b
Mystical433 <peterpanlee2015@gmail.com>
2025-05-22 21:48:30 +0900

    Update startpos.cpp

--- src/hacks/misc/startpos.cpp
+++ src/hacks/misc/startpos.cpp
@@ -4,8 +4,7 @@
 
 using namespace geode::prelude;
 
-class $modify (StartposPlayLayer, PlayLayer)
-{
+class $modify (StartposPlayLayer, PlayLayer) {
     struct Fields {
         std::vector<StartPosObject*> objs;
         int selectedIndex = -1;
@@ -15,19 +14,15 @@ class $modify (StartposPlayLayer, PlayLayer)
         CCSprite* right = nullptr;
         CCLabelBMFont* label = nullptr;
 
-        CCPoint position;
-        float scale;
-        float opacity;
+        float scale = 1.0f;
     };
 
-    void setStartpos(int index)
-    {
+    void setStartpos(int index) {
         if (m_fields->objs.empty())
             return;
 
-        std::sort(m_fields->objs.begin(), m_fields->objs.end(), [](auto* a, auto* b) {
-            return a->getPositionX() < b->getPositionX();
-        });
+        std::sort(m_fields->objs.begin(), m_fields->objs.end(),
+            [](auto* a, auto* b) { return a->getPositionX() < b->getPositionX(); });
 
         if (index < -1)
             index = m_fields->objs.size() - 1;
@@ -46,55 +41,51 @@ class $modify (StartposPlayLayer, PlayLayer)
         if (m_isPracticeMode)
             resetLevelFromStart();
 
+        base_cast<BestPlayLayer*>(this)->m_fields->ignoreBest = true;
+
         resetLevel();
         startMusic();
+
+        base_cast<BestPlayLayer*>(this)->m_fields->ignoreBest = false;
 
         updateUI();
     }
 
-    void onLeft(CCObject*)
-    {
+    void onLeft(CCObject*) {
         setStartpos(m_fields->selectedIndex - 1);
     }
 
-    void onRight(CCObject*)
-    {
+    void onRight(CCObject*) {
         setStartpos(m_fields->selectedIndex + 1);
     }
 
-    void addObject(GameObject* obj)
-    {
-        if (obj->m_objectID == 31)
-        {
+    void addObject(GameObject* obj) override {
+        if (obj->m_objectID == 31) {
             m_fields->objs.push_back(as<StartPosObject*>(obj));
-
-            #ifndef GEODE_IS_MACOS
+#ifndef GEODE_IS_MACOS
             if (as<StartPosObject*>(obj)->m_startSettings && !as<StartPosObject*>(obj)->m_startSettings->m_disableStartPos)
                 m_fields->selectedIndex++;
-            #endif
+#endif
         }
-
         PlayLayer::addObject(obj);
     }
 
-    void createObjectsFromSetupFinished()
-    {
+    void createObjectsFromSetupFinished() override {
         PlayLayer::createObjectsFromSetupFinished();
 
         if (m_fields->menu)
             m_fields->menu->removeFromParent();
 
         m_fields->menu = CCMenu::create();
-        m_fields->menu->setContentSize(ccp(0, 0));
+        m_fields->menu->setContentSize({0, 0});
         m_fields->menu->setPosition(CCDirector::get()->getWinSize().width / 2, 25);
 
-        m_fields->label = CCLabelBMFont::create("1/1", "bigFont.fnt");
+        m_fields->label = CCLabelBMFont::create("among us sex", "bigFont.fnt");
 
         m_fields->left = CCSprite::createWithSpriteFrameName("GJ_arrow_02_001.png");
         m_fields->right = CCSprite::createWithSpriteFrameName("GJ_arrow_02_001.png");
         m_fields->left->setScale(0.65f);
         m_fields->right->setScale(0.65f);
-
         m_fields->right->setFlipX(true);
 
         auto l = CCMenuItemSpriteExtra::create(m_fields->left, this, menu_selector(StartposPlayLayer::onLeft));
@@ -106,26 +97,16 @@ class $modify (StartposPlayLayer, PlayLayer)
 
         m_uiLayer->addChild(m_fields->menu);
 
-        bool enabled = false;
-        if (auto mod = Mod::get()->getSubmodule("startpos-switcher"))
-            enabled = mod->isEnabled();
-
-        m_fields->menu->setVisible(enabled && !m_fields->objs.empty());
+        m_fields->menu->setVisible(!m_fields->objs.empty());
 
         updateUI();
     }
 
-    void updateUI()
-    {
+    void updateUI() {
         if (!m_fields->menu || !m_fields->label)
             return;
 
-        m_fields->position = ccp(
-            Mod::get()->getSavedValue<float>("startpos-position.x", CCDirector::get()->getWinSize().width / 2),
-            Mod::get()->getSavedValue<float>("startpos-position.y", 25)
-        );
         m_fields->scale = Mod::get()->getSavedValue<float>("startpos-scale", 1);
-        m_fields->opacity = Mod::get()->getSavedValue<float>("startpos-opacity", 75.0f / 255.0f);
 
         m_fields->label->setString(fmt::format("{}/{}", m_fields->selectedIndex + 1, m_fields->objs.size()).c_str());
         m_fields->label->limitLabelWidth(100, 0.65f, 0);
@@ -133,7 +114,7 @@ class $modify (StartposPlayLayer, PlayLayer)
         auto action = CCSequence::create(
             CCFadeTo::create(0.1f, 225),
             CCFadeTo::create(0.6f, 225),
-            CCFadeTo::create(0.3f, 255 * m_fields->opacity),
+            CCFadeTo::create(0.3f, 255),
             nullptr
         );
 
@@ -142,84 +123,47 @@ class $modify (StartposPlayLayer, PlayLayer)
         m_fields->right->runAction(action->copy());
     }
 
-    virtual void postUpdate(float dt) override
-    {
+    void postUpdate(float dt) override {
         PlayLayer::postUpdate(dt);
 
-        m_fields->position = ccp(
-            Mod::get()->getSavedValue<float>("startpos-position.x", CCDirector::get()->getWinSize().width / 2),
-            Mod::get()->getSavedValue<float>("startpos-position.y", 25)
-        );
         m_fields->scale = Mod::get()->getSavedValue<float>("startpos-scale", 1);
-        m_fields->opacity = Mod::get()->getSavedValue<float>("startpos-opacity", 1);
 
         if (m_fields->menu) {
-            m_fields->menu->setPosition(m_fields->position);
             m_fields->menu->setScale(m_fields->scale);
-            m_fields->menu->setOpacity(255 * m_fields->opacity);
+            m_fields->menu->setOpacity(255); // fully opaque, no opacity effect
         }
     }
 };
 
-class $modify (UILayer)
-{
-    #ifdef GEODE_IS_WINDOWS
-    virtual void keyDown(cocos2d::enumKeyCodes key) override
-    {
+class $modify (UILayer) {
+#ifdef GEODE_IS_WINDOWS
+    void keyDown(cocos2d::enumKeyCodes key) override {
         UILayer::keyDown(key);
 
-        bool enabled = false;
-        if (auto mod = Mod::get()->getSubmodule("startpos-switcher"))
-            enabled = mod->isEnabled();
-
-        if (!enabled)
-            return;
-
         if (auto pl = PlayLayer::get()) {
-            auto spl = as<StartposPlayLayer*>(pl);
-
             if (key == enumKeyCodes::KEY_Q)
-                spl->setStartpos(spl->m_fields->selectedIndex - 1);
+                as<StartposPlayLayer*>(pl)->setStartpos(as<StartposPlayLayer*>(pl)->m_fields->selectedIndex - 1);
 
-            else if (key == enumKeyCodes::KEY_E)
-                spl->setStartpos(spl->m_fields->selectedIndex + 1);
+            if (key == enumKeyCodes::KEY_E)
+                as<StartposPlayLayer*>(pl)->setStartpos(as<StartposPlayLayer*>(pl)->m_fields->selectedIndex + 1);
         }
     }
-    #else
-    void handleKeypress(cocos2d::enumKeyCodes key, bool down) override
-    {
-        bool enabled = false;
-        if (auto mod = Mod::get()->getSubmodule("startpos-switcher"))
-            enabled = mod->isEnabled();
-
-        if (down && enabled) {
+#else
+    void handleKeypress(cocos2d::enumKeyCodes key, bool down) override {
+        if (down) {
             if (auto pl = PlayLayer::get()) {
-                auto spl = as<StartposPlayLayer*>(pl);
-
                 if (key == enumKeyCodes::KEY_Q)
-                    spl->setStartpos(spl->m_fields->selectedIndex - 1);
+                    as<StartposPlayLayer*>(pl)->setStartpos(as<StartposPlayLayer*>(pl)->m_fields->selectedIndex - 1);
 
-                else if (key == enumKeyCodes::KEY_E)
-                    spl->setStartpos(spl->m_fields->selectedIndex + 1);
+                if (key == enumKeyCodes::KEY_E)
+                    as<StartposPlayLayer*>(pl)->setStartpos(as<StartposPlayLayer*>(pl)->m_fields->selectedIndex + 1);
             }
         }
-
         UILayer::handleKeypress(key, down);
     }
-    #endif
+#endif
 };
 
-class StartposUIDelegate : public ModuleChangeDelegate
-{
-    virtual void initOptionsLayer(CCLayer* options) override
-    {
-    }
-};
-
-$execute
-{
-    Loader::get()->queueInMainThread([] {
-        if (auto mod = Mod::get()->getSubmodule("startpos-switcher"))
-            mod->delegate = new StartposUIDelegate();
-    });
+$execute {
+    // No custom UI delegate or external modules used.
 }
