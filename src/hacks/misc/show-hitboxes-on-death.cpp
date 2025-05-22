cf1d834979ed97ee8762da241640c59669cb6b98
Mystical433 <peterpanlee2015@gmail.com>
2025-05-22 19:08:08 +0900

    Update show-hitboxes-on-death.cpp

--- src/hacks/misc/show-hitboxes-on-death.cpp
+++ src/hacks/misc/show-hitboxes-on-death.cpp
@@ -3,27 +3,36 @@
 
 using namespace geode::prelude;
 
-static void removeNode(CCNode* node) {
-    node->removeFromParentAndCleanup(true);
-}
+class $modify(PlayLayer) {
+    static void onModify(auto& self) {
+        (void)self.setHookPriority("PlayLayer::updateVisibility", -6969);
+    }
 
-class $modify(qol, PlayLayer) {
+    void updateVisibility(float p0) {
+        PlayLayer::updateVisibility(p0);
+
+        if (!m_debugDrawNode)
+            return;
+
+        bool showAlways = Mod::get()->getSettingValue<bool>("show-hitboxes");
+        bool showOnDeath = Mod::get()->getSettingValue<bool>("show-hitboxes-on-death");
+
+        bool shouldVis =
+            (GameManager::sharedState()->getGameVariable("0166") && m_isPracticeMode) ||
+            showAlways ||
+            (showOnDeath && m_player1->m_isDead);
+
+        if (shouldVis)
+            this->updateDebugDraw();
+
+        m_debugDrawNode->setVisible(shouldVis);
+    }
+
     void destroyPlayer(PlayerObject* player, GameObject* cause) {
         PlayLayer::destroyPlayer(player, cause);
 
-        if (!Mod::get()->getSettingValue<bool>("show-hitbox")) return;
-
-        auto rect = player->getObjectRect();
-        auto box = CCLayerColor::create(ccc4(255, 0, 0, 100), rect.size.width, rect.size.height);
-        box->setPosition(rect.origin);
-        box->setZOrder(9999);
-
-        box->runAction(CCSequence::create(
-            CCFadeOut::create(2.0f),
-            CCCallFuncN::create(removeNode),
-            nullptr
-        ));
-
-        this->addChild(box);
+        if (Mod::get()->getSettingValue<bool>("show-hitboxes-on-death")) {
+            this->updateVisibility(0.f); // force refresh visibility logic
+        }
     }
 };
